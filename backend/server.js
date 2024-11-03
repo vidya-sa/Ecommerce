9cf8dd428043de060a8af08f84f46f55379f2859
vidya-sa <tamminenividyasagar14@gmail.comgit config -global user.name vidya-sa>
2024-11-03 22:27:50 +0530

    added axios and api in front end

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,23 @@
 import express from 'express';
+import dotenv from 'dotenv'
 import products from './data/products.js'
-const port = 8000;
+dotenv.config();
+const port = process.env.PORT ;
 
 const app = express()
 
-app.get('/',(req,res)=>{
-    res.send('API is running')
-})
+// app.get('/',(req,res)=>{
+//     res.send('API is running')
+// })
 
-app.get('/api/products',(req,res)=>{
-    res.json(products)
-})
-
+app.get('/api/products', (req, res) => {
+    try {
+      res.json(products);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+      res.status(500).json({ message: "Server Error" });
+    }
+  });
 
 
 app.listen(port,()=> console.log(`server is running on port ${port}`))
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,22 +1,34 @@
-import React from 'react'
-import {Row,Col} from 'react-bootstrap'
-import Product from '../components/Product'
+import React, { useEffect, useState } from "react";
+import axios from 'axios'
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
 
 const Homepage = () => {
-    let products =[]
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => { // Corrected function name
+      try {
+        const { data } = await axios.get('/api/products');
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+    fetchProducts();
+  }, []);
+  
   return (
     <>
-    <h1>Latest Products</h1>
-    <Row>
-    {products.map((product) => (
-        <Col key={product._id}sm={12} md={6} lg={4} xl={3}>
-            <Product product={product}/>
-        </Col>
-  ))}
-    </Row>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
     </>
-    
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
